9dfe9b8492a61375a6273c559495cc6afae34d1e
Dan Koskei <qimtaid@gmail.com>
2024-04-23 17:59:51 +0300

    complete lab

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,19 @@
 import React from "react";
 import { name, city } from "../data/data.js";
 
+const hStyle = {
+  color: "firebrick",
+  textAlign: "center"
+}
+
 function Home() {
   // update the JSX being returned!
-  return <div>Home</div>;
+  return (
+    <div>
+      <h1 style={hStyle}>{name} is a Web Developer from {city}</h1>
+    
+    </div>
+  )
 }
 
 export default Home;
